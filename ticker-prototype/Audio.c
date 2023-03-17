85685abde7965f93c4f868c7049b53c3f85ba8c9
Benjamin Smus <bensmus@gmail.com>
2023-03-16 21:52:48 -0700

    audio sample verification

--- ticker-prototype/Audio.c
+++ ticker-prototype/Audio.c
@@ -16,7 +16,7 @@ void Audio_init()
 	// Open the PCM output
 	int err = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
 	if (err < 0) {
-		printf("Play-back open error: %s\n", snd_strerror(err));
+		printf("ERROR: Audio_init: Play-back open error: %s\n", snd_strerror(err));
 		exit(EXIT_FAILURE);
 	}
 
@@ -29,7 +29,7 @@ void Audio_init()
 			1,			// Allow software resampling
 			50000);		// 0.05 seconds per buffer
 	if (err < 0) {
-		printf("Play-back configuration error: %s\n", snd_strerror(err));
+		printf("ERROR: Audio_init: Play-back configuration error: %s\n", snd_strerror(err));
 		exit(EXIT_FAILURE);
 	}
 }
@@ -37,38 +37,48 @@ void Audio_init()
 // Copy WAV contents into wavedata_t->pData.
 // Size of pData array depends on how long the WAV file is.
 // !! Client code must free memory in wavedata_t !!
-void Audio_load(char *fileName, wavedata_t *pWaveData, int samplesToLoad)
+// Returns the number of samples loaded OR the maximum number of samples that can be loaded.
+int Audio_load(char *fileName, wavedata_t *pWaveData, int sampleCountRequested)
 {
 	assert(pWaveData);
-
-	// Wave file has 44 bytes of header data. This code assumes file
-	// is correct format.
-	const int DATA_OFFSET_INTO_WAVE = 44;
 
 	// Open file
 	FILE *file = fopen(fileName, "r");
 	if (file == NULL) {
-		fprintf(stderr, "ERROR: Unable to open file %s.\n", fileName);
+		fprintf(stderr, "ERROR: Audio_load: Unable to open file %s.\n", fileName);
 		exit(EXIT_FAILURE);
 	}
 
-	int sizeInBytes;
-	if (samplesToLoad == -1) // Load them all.
-	{
-		// Get file size
-		fseek(file, 0, SEEK_END);
-		sizeInBytes = ftell(file) - DATA_OFFSET_INTO_WAVE;
-		pWaveData->numSamples = sizeInBytes / SAMPLE_SIZE;
+	// Check that the first 4 bytes are RIFF
+	char *riff = "RIFF";
+	char riff_test[5];
+	fread(riff_test, 1, 4, file);
+	riff_test[4] = '\0';
+	if (strcmp(riff, riff_test) != 0) {
+		fprintf(stderr, "WARNING: Audio_load: %s is not WAV or is corrupted.\n", fileName);
+	}
+
+	const int DATA_OFFSET_INTO_WAVE = 44;
+	fseek(file, 0, SEEK_END);
+	int sampleCountMax = (ftell(file) - DATA_OFFSET_INTO_WAVE) / SAMPLE_SIZE;
+
+	if (sampleCountRequested == -1) {
+		// Load them all.
+		pWaveData->numSamples = sampleCountMax;
 	} else {
-		sizeInBytes = samplesToLoad * SAMPLE_SIZE;
-		pWaveData->numSamples = samplesToLoad;
+		if (sampleCountRequested > sampleCountMax) {
+			fprintf(stderr, "WARNING: Could not load %d samples from %s: file too short.\n", sampleCountRequested, fileName);
+			fclose(file);
+			return sampleCountMax;
+		}
+		pWaveData->numSamples = sampleCountRequested;
 	}
 
 	// Allocate Space
-	pWaveData->pData = malloc(sizeInBytes);
+	int numBytes = pWaveData->numSamples * SAMPLE_SIZE;
+	pWaveData->pData = malloc(numBytes);
 	if (pWaveData->pData == NULL) {
-		fprintf(stderr, "ERROR: Unable to allocate %d bytes for file %s.\n",
-				sizeInBytes, fileName);
+		fprintf(stderr, "ERROR: Audio_load: Unable to allocate %d bytes for file %s.\n", numBytes, fileName);
 		exit(EXIT_FAILURE);
 	}
 
@@ -76,12 +86,13 @@ void Audio_load(char *fileName, wavedata_t *pWaveData, int samplesToLoad)
 	fseek(file, DATA_OFFSET_INTO_WAVE, SEEK_SET);
 	int samplesRead = fread(pWaveData->pData, SAMPLE_SIZE, pWaveData->numSamples, file);
 	if (samplesRead != pWaveData->numSamples) {
-		fprintf(stderr, "ERROR: Unable to read %d samples from file %s (read %d).\n",
+		fprintf(stderr, "ERROR: Audio_load: Unable to read %d samples from file %s (read %d).\n",
 				pWaveData->numSamples, fileName, samplesRead);
 		exit(EXIT_FAILURE);
 	}
 
 	fclose(file);
+	return pWaveData->numSamples;
 }
 
 // Play wavedata
@@ -92,23 +103,16 @@ void Audio_play(wavedata_t *pWaveData)
 
 	// Write data and play sound (blocking)
 	snd_pcm_sframes_t frames = snd_pcm_writei(handle, pWaveData->pData, pWaveData->numSamples);
-
-	// if (frames == -EPIPE)
-	// {
-	// 	puts("Underrun from snd_pcm_writei");
-	// } else {
-	// 	printf("%ld\n", frames);
-	// }
 	
 	// Check for errors
 	if (frames < 0)
 		frames = snd_pcm_recover(handle, frames, 0);
 	if (frames < 0) {
-		fprintf(stderr, "ERROR: Failed writing audio with snd_pcm_writei(): %li\n", frames);
+		fprintf(stderr, "ERROR: Audio_play: Failed writing audio with snd_pcm_writei(): %li\n", frames);
 		exit(EXIT_FAILURE);
 	}
 	if (frames > 0 && frames < pWaveData->numSamples)
-		printf("Short write (expected %d, wrote %li)\n", pWaveData->numSamples, frames);
+		printf("WARNING: Audio_play: Short write (expected %d, wrote %li)\n", pWaveData->numSamples, frames);
 }
 
 void Audio_cleanup()
--- ticker-prototype/Ticker.c
+++ ticker-prototype/Ticker.c
@@ -1,8 +1,9 @@
 #define TICK_FILE "metronome-tick.wav"
-#define TICK_SAMPLE_COUNT 700 // How many samples long the tick should be.
+#define TICK_SAMPLE_COUNT 700 // How many samples long the tick should be. (reasonable value: 700)
 
 #include <assert.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include "Audio.h"
 #include "State.h"
 
@@ -11,12 +12,15 @@ static pthread_t tickerRoutineThread;
 
 static wavedata_t tick;
 static wavedata_t silence;
+static bool initialized = false;
 
 static void *tickerRoutine(void *args)
 {
+    puts("In tickerRoutine!");
     while (1)
     {
         int bpm = State_getBpm();
+        printf("bpm= %d\n", bpm);
         silence.numSamples = SAMPLE_RATE * (60.0 / bpm);
         Audio_play(&tick);
         Audio_play(&silence);
@@ -26,23 +30,27 @@ static void *tickerRoutine(void *args)
 
 // Relies on Audio and State being initialized
 void Ticker_init()
-{
+{   
+    int numSamples = Audio_load(TICK_FILE, &tick, TICK_SAMPLE_COUNT);
+    if (numSamples != TICK_SAMPLE_COUNT) return; // Requested too many samples
+
     int maxSamples = SAMPLE_RATE * (60.0 / MIN_BPM); // At most 60 seconds of silence (1bpm)
     zeros = malloc(SAMPLE_SIZE * maxSamples);
 	memset(zeros, 0, SAMPLE_SIZE * maxSamples);
-    
-    Audio_load(TICK_FILE, &tick, TICK_SAMPLE_COUNT);
     
     silence.numSamples = maxSamples;
     silence.pData = zeros;
 
     pthread_create(&tickerRoutineThread, NULL, tickerRoutine, NULL);
+    initialized = true;
 }
 
 void Ticker_cleanup()
 {
-    pthread_cancel(tickerRoutineThread);
-    pthread_join(tickerRoutineThread, NULL);
-    free(tick.pData);
-    free(silence.pData);
+    if (initialized) {
+        pthread_cancel(tickerRoutineThread);
+        pthread_join(tickerRoutineThread, NULL);
+        free(tick.pData);
+        free(silence.pData);
+    }
 }
