76c4a873dc755f85103a21cb83b16e41f96627c6
Benjamin Smus <bensmus@gmail.com>
2023-03-16 22:24:48 -0700

    basic spi testing

---
+++ spi-prototype/spi.c
@@ -0,0 +1,57 @@
+#include <fcntl.h>
+#include <linux/spi/spidev.h>
+#include <stdint.h>
+
+// Assume pins already configured for SPI
+// E.g. for SPI1, CS0:
+// (bbg)$ config-pin P9_28 spi_cs
+// (bbg)$ config-pin P9_31 spi_sclk
+// (bbg)$ config-pin P9_29 spi
+// (bbg)$ config-pin P9_30 spi
+#define SPI_DEV_BUS0_CS0 "/dev/spidev0.0"
+#define SPI_DEV_BUS1_CS0 "/dev/spidev1.0"
+#define SPI_DEV_BUS1_CS1 "/dev/spidev1.1"
+#define SPI_MODE_DEFAULT 0
+#define SPEED_HZ_DEFAULT 500000 // Arbitrary, but 500000 is reasonable
+
+int SPI_initPort(char* spiDevice)
+{
+    // Open Device
+    int spiFileDesc = open(spiDevice, O_RDWR);
+    if (spiFileDesc < 0) {
+        printf("Error: Can't open device %s\n", spiDevice);
+        exit(1);
+    }
+
+    // Set port parameters
+    // Set SPI mode: Necessary
+    int spiMode = SPI_MODE_DEFAULT;
+    int err = ioctl(spiFileDesc, SPI_IOC_WR_MODE, &spiMode);
+    if (err < 0) {
+        printf("Error: Set SPI mode failed\n");
+        exit(1);
+    }
+
+    return spiFileDesc;
+}
+
+void SPI_transfer(int spiFileDesc, uint8_t *sendBuf, uint8_t *receiveBuf, int length)
+{
+    // Setting transfer this way ensures all other fields are 0
+    struct spi_ioc_transfer transfer = {
+        .tx_buf = (unsigned long) sendBuf,
+        .rx_buf = (unsigned long) receiveBuf,
+        .len = length
+    };
+
+    const int NUM_TRANSFERS = 1;
+    int status = ioctl(spiFileDesc, SPI_IOC_MESSAGE(NUM_TRANSFERS), &transfer);
+    if (status < 0) {
+        printf("Error: SPI Transfer failed\n");
+    }
+}
+
+int main(void)
+{
+    SPI_initPort(SPI_DEV_BUS0_CS0);
+}
