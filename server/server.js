d90a7d7602acf1f85b6e43189937cbb7a60f17f4
Benjamin Smus <bensmus@gmail.com>
2023-03-30 12:31:42 -0700

    file upload works: minor changes will be required for beaglebone

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // https://progressivecoder.com/serve-static-html-files-in-nodejs-without-express/
+// https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp
 
 const port = 3000;
 
@@ -8,6 +9,7 @@ const fs = require('fs');
 
 //Third-Party Modules
 const httpStatus = require('http-status-codes');
+const formidable = require('formidable');
 
 server = http.createServer((request, response) => {
     if (request.url === "/") {
@@ -18,7 +20,12 @@ server = http.createServer((request, response) => {
         })
     } else if (request.url === "/file-submit") {
         // Handle file submission
-
+        const form = new formidable.IncomingForm()
+        form.parse(request, (err, fields, files) => {
+            const tmpFilePath = files['audio-sample'].filepath;
+            // console.log(filename); // "/tmp/0e1f004f1355cfe6e54daa000" in the /tmp folder on my Linux file system
+            fs.rename(tmpFilePath, "./uploaded/audio-sample.wav", (err) => {if (err) throw err});
+        })
         // Redirect to homepage
         response.writeHead(302,  {Location: "/"})
         response.end();
