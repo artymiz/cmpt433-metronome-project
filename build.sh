9e9f1d0bcd42be1f38354396fe17b179fa41a328
Connor <connor@debian.connor-433.com>
2023-03-30 22:45:54 -0700

    Updated build, clean, and make files to generate .o files in the same directory, and copy the .o files to the obj folder

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ echo "\e[40m\e[92m [Making and binning o files from ticker-prototype] \e[0m"
 make -f ticker-prototype/Makefile
 
 echo "\e[40m\e[92m [Linking libraries in bin and sending to target] \e[0m"
-make -f bin/Makefile
+make -f obj/Makefile
 
--- clean.sh
+++ clean.sh
@@ -4,4 +4,4 @@ echo "\e[40m\e [36mCleaning project] \e[0m"
 rm button-prototype/*.o
 rm ticker-prototype/*.o
 rm spi-prototype/*.o
-rm bin/*.o
+rm obj/*.o
