8ee4fd57624afb1b1633a193c4ccfbd3201d6a45
arty <arty@sfu.ca>
2023-04-02 16:24:26 -0700

    letter test

--- spi-prototype/Graphics_test.c
+++ spi-prototype/Graphics_test.c
@@ -1,13 +1,29 @@
 #include  "Graphics.h"
+#include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
     Graphics_init();
-    Graphics_writeChar('A', 2, 0, 0);
+
+    // Graphics_writeChar('A', 2, 120, 160);
     // Graphics_writeChar('A', 2, 10, 10);
-    // Graphics_writeChar('A', 3, 50, 50);
     //Graphics_writeChar('A', 4, 100, 100);
-    //Graphics_writeStr("...", 1, 50, 10);
+    // char buf[4];
+    // for (int i = 0; i < 150; i+=9) {
+    //     sprintf(buf, "%d", i);
+    //     Graphics_writeStr(buf, 5, 50, 10);
+    //     usleep(300000);
+    // }
+    for (unsigned char i = 32; i < 128; i++) {
+        Graphics_writeChar(i, 1, 120, 280);
+        Graphics_writeChar(i, 2, 120, 250);
+        Graphics_writeChar(i, 3, 120, 210);
+        Graphics_writeChar(i, 4, 120, 160);
+        Graphics_writeChar(i, 6, 120, 40);
+        usleep(350000);
+    }
+    //Graphics_writeStr("144", 5, 50, 10);
     //Graphics_writeStr("...", 3, 100, 10);
     Graphics_cleanup();
     return 0;
